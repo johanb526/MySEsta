6c70e88ebc81367be6e4b5f195e4462bd33c5bad
johanb526 <johanb526@gmail.com>
2023-02-13 14:00:21 -0500

    Proyecto Dashboard

---
+++ Actividad1.R
@@ -0,0 +1,11 @@
+install.packages("flexdashboard")  # instala paquete flexdashboard
+install.packages("devtools")       # instala paquete devtools
+
+devtools::install_github("dgonxalex80/paqueteMET",force=TRUE) # descarga de paqueteMET
+
+library(paqueteMET)                # activar paqueteMET
+
+data("CarreraLuz22")               # cargar la dataset
+
+install.packages("remotes")  
+remotes::install_github("rstudio/bslib",force=TRUE)  # descarga paquete que contiene formato    
